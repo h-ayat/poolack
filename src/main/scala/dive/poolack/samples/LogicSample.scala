670e01077f2a458124c40929d9af6465132e49b4
S.H.Ayat <fahim.ayat@gmail.com>
2024-04-14 19:43:19 +0330

    add variance

--- src/main/scala/dive/poolack/samples/LogicSample.scala
+++ src/main/scala/dive/poolack/samples/LogicSample.scala
@@ -4,28 +4,37 @@ import scala.concurrent.Future
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class FE[E, A](in: Future[Either[E, A]]) {
+class FE[+E, A](val in: Future[Either[E, A]]) {
 
-  def map[B](f: A => B): FE[E, B] = ???
+  def map[B](f: A => B): FE[E, B] =
+    new FE(in.map(_.map(f)))
 
-  def flatMap[B](f: A => FE[E, B]): FE[E, B] = ???
+  def flatMap[B, E2 >: E](f: A => FE[E2, B]): FE[E2, B] = {
+    val result = in.flatMap {
+      case Right(a) =>
+        f(a).in
+      case Left(e) =>
+        Future.successful(Left(e))
+    }
+    new FE(result)
+  }
 
 }
 
 object FE {
-  def pure[E, A](a: A): FE[E, A] = ???
+  def pure[E, A](a: A): FE[E, A] = new FE(Future.successful(Right(a)))
 
-  def fail[E, A](e: E): FE[E, A] = ???
+  def fail[E, A](e: E): FE[E, A] = new FE(Future.successful(Left(e)))
 }
 
 object LogicSample {
 
   private def authorize(
       session: Session
-  ): FE[Error, UserId] =
+  ): FE[Error.UnAuthorized.type, UserId] =
     ???
 
-  private def getAccounts(userId: UserId): FE[Error, List[Account]] = ???
+  private def getAccounts(userId: UserId): FE[Nothing, List[Account]] = ???
 
   private def getAccountById(
       accountId: AccountId
@@ -34,15 +43,15 @@ object LogicSample {
   private def getSourceAccount(
       userId: UserId,
       accountId: AccountId
-  ): FE[Error, Account] = {
+  ): FE[Error.Forbidden.type, Account] = {
     getAccounts(userId).flatMap { p =>
       p.find(_.id == accountId) match {
         case None =>
           // new FE[Error, Account](Future.successful(Left(Forbidden)))
-          FE.fail[Error, Account](Forbidden)
+          FE.fail[Error.Forbidden.type, Account](Error.Forbidden)
         case Some(value) =>
           // FE.pure(value)
-          FE.pure[Error, Account](value)
+          FE.pure[Error.Forbidden.type, Account](value)
         // new FE[Error, Account](Future.successful(Right(value)))
       }
     }
@@ -51,7 +60,7 @@ object LogicSample {
   private def validateTransactionLimit(
       accountId: AccountId,
       amount: Rial
-  ): FE[TransactionLimitExceeded, Unit] = ???
+  ): FE[Error.TransactionLimitExceeded, Unit] = ???
 
   private def reallyExecuteTransaction(
       from: AccountId,
@@ -59,7 +68,21 @@ object LogicSample {
       amount: Rial
   ): Serial = ???
 
+
+
   def exec(req: Request): FE[Error, Ok] = {
+
+    val a : FE[Nothing, Session] = ???
+
+    val result2: FE[Error.UnAuthorized.type,UserId] = for {
+      session <- a
+      x <- authorize(session)
+    } yield x
+
+    val result = authorize(req.session).flatMap(userid =>
+      getSourceAccount(userId = userid, req.myAccount)
+    )
+
     val o = for {
       userId <- authorize(req.session)
       account <- getSourceAccount(userId, req.myAccount)
@@ -116,11 +139,24 @@ final case class Serial(value: String) extends AnyVal
 final case class Ok(serial: Serial)
 
 sealed trait Error extends Product with Serializable
-case object UnAuthorized extends Error
-case object Forbidden extends Error
-case class InvalidAccountId(accountId: AccountId) extends Error
-case class InsufficientFunds(remaining: Rial) extends Error
-case class TransactionLimitExceeded(rule: String) extends Error
+object Error {
+  case object UnAuthorized extends Error
+  case object Forbidden extends Error
+  case class InvalidAccountId(accountId: AccountId) extends Error
+  case class InsufficientFunds(remaining: Rial) extends Error
+  case class TransactionLimitExceeded(rule: String) extends Error
 // 1 unauthorized
 // 2 folan
 // 3
+}
+
+
+
+
+sealed trait LL
+object EmptyLL extends LL
+final case class Node(value: Int) extends LL
+
+
+
+
